b43826c514c40034d175a3c3eef2047e04e37414
Haizhilingyu <haizhilingyu@gmail.com>
2023-08-29 12:06:16 +0800

    Update config.ts

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -49,6 +49,7 @@ export default defineUserConfig({
 		  {
 			text: '常用命令',
 			children: [
+		          { text: 'shell', link: '/docs/skill/shell' },
 			  { text: 'git', link: '/docs/skill/git' },
 			  { text: 'ssh', link: '/docs/skill/ssh' },
 			],
